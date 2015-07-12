ffaad5a20d97c7bbc4ae1e98d01a77733cd9e79b
wwselleck <wwselleck@gmail.com>
2015-07-12 02:57:41 -0500

    Fix travis for node v <=8

--- test/mantest.js
+++ test/mantest.js
@@ -7,6 +7,6 @@ trello.getBoards({filter: 'open'}).then(function(boards){
 		trello.getListsOfBoard(boards[0].id).then(function(lists){
 			trello.addCardToList(lists[0].id, 'TestCard1').then(function(card){
 				console.log(card);
-			})
+			});
 		});
 });
