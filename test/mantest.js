ca983ff5bca40cb8a14d522ed6bfa2adffc00cc9
wwselleck <wwselleck@gmail.com>
2015-07-20 01:31:06 -0500

    completeChecklistItem, among others

--- test/mantest.js
+++ test/mantest.js
@@ -1,17 +1,23 @@
 var Trellode = require('../index.js');
 var Settings = require('./settings.json');
 
-var trello = new Trellode(Settings.trello_key, Settings.trello_token);
+var trello = new Trellode(Settings.trello_key, Settings.test_trello_token);
 
-trello.getBoards(function(err, data){
-	console.log(err);
-	console.log(data);
-});
 
 trello.getBoards({filter: 'open'}).then(function(boards){
 		trello.getListsOfBoard(boards[0].id).then(function(lists){
-			trello.addCardToList(lists[0].id, 'TestCard1').then(function(card){
-				console.log(card);
+			var list = lists[1];
+			trello.getCardsOfList(list.id).then(function(cards){
+				var card = cards[0];
+				trello.getChecklistsOfCard(card.id).then(function(checklists){
+					var checklist = checklists[0];
+					console.log(card);
+					console.log(checklist);
+					console.log(checklist.checkItems[0].id);
+					trello.completeChecklistItem(checklist.checkItems[0].id, checklist.id, card.id).then(function(res){
+						console.log(res);
+					});
+				});	
 			});
 		});
 });
